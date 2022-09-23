8726634bcf8f8d54101f526a766cc883f1edf40a
Dipta Dhananjaya <madedipta88@gmail.com>
2022-09-23 20:07:55 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Menu from "./components/Menu";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return <Menu />;
+};
+
+export default App;
